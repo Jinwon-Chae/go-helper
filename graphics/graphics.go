4151f67efd9ebeaf420a1190c4b28057e6f1dad3
jwchae <jwchae@laonroad.com>
2024-03-04 15:21:10 +0900

    Feat: png 수평&수직 concat 기능 추가

--- graphics/graphics.go
+++ graphics/graphics.go
@@ -3,6 +3,8 @@ package graphics
 import (
 	"errors"
 	"fmt"
+	"image"
+	"math"
 
 	"github.com/fogleman/gg"
 )
@@ -15,6 +17,7 @@ func NewGraphics() *Graphics {
 	return &Graphics{}
 }
 
+// 최초 배경 이미지 생성 및 설정
 // @param imageArea ImageArea(strct) 배경 이미지 가로 세로 픽셀 수
 // @param color Color(struct) 배경 이미지 색
 func (g *Graphics) Open(imageArea ImageArea, color Color) (err error) {
@@ -31,10 +34,12 @@ func (g *Graphics) Open(imageArea ImageArea, color Color) (err error) {
 	return
 }
 
+// 인스턴스 명시적 소멸
 func (g *Graphics) Close() {
 	g.ptr = nil
 }
 
+// 텍스트 설정
 // @param config TextConfig(strct) 텍스트 설정 값
 // @param text string 텍스트 내용
 func (g *Graphics) WriteText(config TextConfig, text string) (err error) {
@@ -48,7 +53,91 @@ func (g *Graphics) WriteText(config TextConfig, text string) (err error) {
 	return
 }
 
+// 기존 설정 초기화
+func (g *Graphics) Clear() {
+	g.ptr.Clear()
+}
+
+// 새롭게 배경 이미지 색 설정
+func (g *Graphics) SetBackgroundColor(color Color) {
+	g.ptr.SetRGB(color.Red, color.Green, color.Blue)
+	g.ptr.Clear()
+}
+
+// 이미지 저장
 // @param name string 저장 이미지 이름(확장자 제외)
 func (g *Graphics) SavePNG(name string) (err error) {
 	return g.ptr.SavePNG(fmt.Sprintf("%s.png", name))
+}
+
+// 이미지 수평 병합
+// @param name string 병합된 파일 이름(확장자 제외)
+// @param paths ...string 수평 평합 대상 이미지 경로들
+func HorizontalConcatPNG(name string, paths ...string) (err error) {
+	if len(paths) < 1 {
+		return errors.New("path is empty")
+	}
+
+	var width int
+	var height int
+	var images []image.Image
+	for _, path := range paths {
+		i, err := gg.LoadPNG(path)
+		if err != nil {
+			return errors.New("png load fail: " + path)
+		}
+
+		s := i.Bounds().Size()
+
+		width += s.X
+		height = int(math.Max(float64(height), float64(s.Y)))
+
+		images = append(images, i)
+	}
+
+	var beforeX int
+	dc := gg.NewContext(width, height)
+	for _, image := range images {
+		dc.DrawImage(image, beforeX, 0)
+		beforeX = image.Bounds().Size().X
+	}
+	dc.SavePNG(name)
+
+	return
+}
+
+// 이미지 수직 병합
+// @param name string 병합된 파일 이름(확장자 제외)
+// @param paths ...string 수직 평합 대상 이미지 경로들
+func VerticalConcatPNG(name string, paths ...string) (err error) {
+	if len(paths) < 1 {
+		return errors.New("path is empty")
+	}
+
+	var width int
+	var height int
+	var images []image.Image
+	for _, path := range paths {
+		i, err := gg.LoadPNG(path)
+		if err != nil {
+			return errors.New("png load fail: " + path)
+		}
+
+		s := i.Bounds().Size()
+
+		width = int(math.Max(float64(width), float64(s.X)))
+		height += s.Y
+
+		images = append(images, i)
+	}
+
+	var beforeY int
+	dc := gg.NewContext(width, height)
+	for _, image := range images {
+		dc.DrawImage(image, 0, beforeY)
+		beforeY = image.Bounds().Size().Y
+	}
+	dc.SavePNG(name)
+
+	return
 }
--- graphics/graphics_test.go
+++ graphics/graphics_test.go
@@ -23,6 +23,7 @@ func TestDrawText(t *testing.T) {
 	err = g.WriteText(config, text1)
 	assert.Nil(t, err)
 
+	c = Color{Red: 1, Green: 1, Blue: 0}
 	p = Position{X: 40, Y: 20}
 	config = TextConfig{Color: c, Font: NanumGothicBold, Size: 20, Position: p}
 	text2 := "고통"
@@ -30,7 +31,48 @@ func TestDrawText(t *testing.T) {
 	err = g.WriteText(config, text2)
 	assert.Nil(t, err)
 
-	file := "out"
+	file := "out2"
 	err = g.SavePNG(file)
+	assert.Nil(t, err)
+}
+
+func TestConcat(t *testing.T) {
+	g := NewGraphics()
+	defer g.Close()
+	assert.NotNil(t, g)
+
+	i := ImageArea{Width: 160, Height: 32}
+	c := Color{Red: 0, Green: 0, Blue: 0} // 검정
+	err := g.Open(i, c)
+	assert.Nil(t, err)
+
+	c = Color{Red: 1, Green: 1, Blue: 1}
+	p := Position{X: 0, Y: 20}
+	config := TextConfig{Color: c, Font: NanumGothicBold, Size: 20, Position: p}
+	text1 := "교통"
+	err = g.WriteText(config, text1)
+	assert.Nil(t, err)
+
+	file := "out1"
+	err = g.SavePNG(file)
+	assert.Nil(t, err)
+
+	g.Clear()
+	g.SetBackgroundColor(Color{Red: 0, Green: 0, Blue: 0})
+	c = Color{Red: 1, Green: 1, Blue: 0}
+	p = Position{X: 40, Y: 20}
+	config = TextConfig{Color: c, Font: NanumGothicBold, Size: 20, Position: p}
+	text2 := "고통"
+	err = g.WriteText(config, text2)
+	assert.Nil(t, err)
+
+	file = "out2"
+	err = g.SavePNG(file)
+	assert.Nil(t, err)
+
+	err = HorizontalConcatPNG("text1.png", "out1.png", "out2.png")
+	assert.Nil(t, err)
+
+	err = VerticalConcatPNG("test.png", "out1.png", "out2.png")
 	assert.Nil(t, err)
 }
